3b7f2fb3ff6788c07a706c9a91f83a9186be9f46
jmelcher <jonathan@bluewiretech.com>
2018-07-09 13:08:05 -0500

    Added some config for adding users

--- ncart_app/admin.py
+++ ncart_app/admin.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 from models import Project
 from models import Port
 from models import Firewall
-from users.models import User
+from users.models import UserProfile
 
 
 admin.site.register(Project)
 admin.site.register(Port)
 admin.site.register(Firewall)
-admin.site.register(User)
+admin.site.register(UserProfile)
 
 
--- users/models.py
+++ users/models.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(verbose_name='Username', max_length=15)
+class UserProfile(models.Model):
+    user_auth = models.OneToOneField(User, primary_key=True)
     firstname = models.CharField(verbose_name='First Name', max_length=15)
     lastname = models.CharField(verbose_name='Last Name', max_length=15)
     email = models.EmailField(verbose_name='Email',null=True, default=None)
@@ -13,4 +14,4 @@ class User(models.Model):
     access = models.CharField(verbose_name='Access Level', max_length=10)
 
     def __unicode__(self):
-        return self.username
+        return self.user_auth.username
