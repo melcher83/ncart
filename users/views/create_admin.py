ec441c24c45fb0ca377fc23fa5c5dbf67ffe93f5
jmelcher <jonathan@bluewiretech.com>
2018-07-10 08:32:49 -0500

    Added Username

--- users/views/create_admin.py
+++ users/views/create_admin.py
@@ -21,8 +21,20 @@ def page(request):
     form = Form_admin()
     print 'TEST'
     form = Form_admin()
+    x=UserProfile.objects.filter()
+    list = []
+
+    for item in x:
+        list.append(UserProfile.objects.get(user_auth=item))
+    form.fields['user_auth'].initial=len(list)+1
+
+
+
+
     return render(request, 'create_admin.html', {'form' : form})
 class Form_admin(forms.ModelForm):
+
+
 
   user_auth = forms.CharField(label='User Auth', max_length=30, help_text='Try to make this Unique',widget=forms.HiddenInput())
   username = forms.CharField(label='Username', max_length=30, help_text='Try to make this Unique')
