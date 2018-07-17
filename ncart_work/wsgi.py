0639c52fa57e02d43a8b451972d2c516bbadf53e
jmelcher <jonathan@bluewiretech.com>
2018-07-17 15:33:11 -0500

    changed docker file

--- ncart_work/wsgi.py
+++ ncart_work/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ncart project.
+WSGI config for ncart_work project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ncart.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ncart_work.settings")
 
 application = get_wsgi_application()
