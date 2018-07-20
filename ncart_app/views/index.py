a45ad7ecb82a5f66cd996f273fa5a692160f6c28
jmelcher <jonathan@bluewiretech.com>
2018-07-20 09:09:08 -0500

    Added in project details view

--- ncart_app/views/index.py
+++ ncart_app/views/index.py
@@ -2,4 +2,4 @@ from ncart_app.models import Project
 from django.shortcuts import render
 def page(request):
   all_projects = Project.objects.all()
-  return render(request, 'en/public/index.html', {'action': "Display all projects", 'all_projects': all_projects})
+  return render(request, 'index.html', {'action': "Display all projects", 'all_projects': all_projects})
